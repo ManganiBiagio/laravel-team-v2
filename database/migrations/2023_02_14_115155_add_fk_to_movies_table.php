f2ad5b019f35f7e4cfaa644e898b6e7a37f24150
ManganiBiagio <biagiomangani@live.it>
2023-02-14 13:01:32 +0100

    completed v2

---
+++ database/migrations/2023_02_14_115155_add_fk_to_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->unsignedBigInteger("production_company_id")->nullable();
+            $table->foreign("production_company_id")->references("id")->on("production_companies");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropForeign("movies_production_company_id_foreign");
+            $table->dropColumn("production_company_id");
+        });
+    }
+};
